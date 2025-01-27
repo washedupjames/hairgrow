13ba723289a6cde813e413c42d51d285cd7181a2
James <parkerman23@gmail.com>
2025-01-27 02:41:33 +0000

    third

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,13 +17,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+#CSRF_TRUSTED_ORIGINS =  ['']
 
 # Application definition
 
